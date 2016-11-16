f9dbe31dd7a6ceeb4a6ac2af080c47773b8aefc6
Richard Price <mrspondooli@gmail.com>
2016-11-16 23:45:55 +0000

    added IBoutlets for PostCell, added tableView to FeedVC hooked up delegate and datasource

---
+++ OnePost/PostCell.swift
@@ -0,0 +1,30 @@
+//
+//  PostCell.swift
+//  OnePost
+//
+//  Created by Richard Price on 16/11/2016.
+//  Copyright © 2016 twisted echo. All rights reserved.
+//
+
+import UIKit
+
+class PostCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var usernameLbl: UILabel!
+    @IBOutlet weak var profileImage:UIImageView!
+    @IBOutlet weak var postImg: UIImageView!
+    @IBOutlet weak var timeAgoLbl: UILabel!
+    @IBOutlet weak var likesImg: UIImageView!
+    @IBOutlet weak var commentsImg: UIImageView!
+    @IBOutlet weak var searchImg: UIImageView!
+    @IBOutlet weak var makePostImg: UIImageView!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+       
+    }
+
+
+}
