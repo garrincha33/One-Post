32e95a9d24185ccc50e2dbc37a153ca07290bd6d
Richard Price <mrspondooli@gmail.com>
2016-11-19 10:29:32 +0000

    added imagepicker delegate and info plist security to crepost VC

--- OnePost/CreatePostVC.swift
+++ OnePost/CreatePostVC.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class CreatePostVC: UIViewController {
+class CreatePostVC: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
     
     @IBOutlet weak var profileImg: UIImageView!
     
@@ -17,12 +17,17 @@ class CreatePostVC: UIViewController {
     @IBOutlet weak var makePost: UITextField!
     
     @IBOutlet weak var addImg: UIImageView!
+    
+    var imagePicker: UIImagePickerController!
  
     override func viewDidLoad() {
         super.viewDidLoad()
-
-  
         
+        imagePicker = UIImagePickerController()
+        imagePicker.allowsEditing = true
+        imagePicker.delegate = self
+        
+
     }
 
     @IBAction func postBtnPressed(_ sender: Any) {
@@ -36,12 +41,33 @@ class CreatePostVC: UIViewController {
     
     
     @IBAction func cancelBtnPressed(_ sender: Any) {
-        
-        
+     
         dismiss(animated: true, completion: nil)
+
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
         
+        if let image = info[UIImagePickerControllerEditedImage] as? UIImage {
+            
+            addImg.image = image
+            
+            
+        } else {
+            
+            print("RICH: a valid image wasnt selected")
+        }
+        
+        imagePicker.dismiss(animated: true, completion: nil)
         
     }
+    
+    @IBAction func addImgTapped(_ sender: Any) {
+        
+        present(imagePicker, animated: true, completion: nil)
+        
+    }
+    
    
 
 
